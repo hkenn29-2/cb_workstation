7b6e61d9318fe0df09ff87b66e78c3c50d39b0c9
hkenn29 <hkenn29@outlook.com>
2020-12-06 04:55:28 +0000

    More changes

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -9,7 +9,12 @@ package 'git'
 package 'ntp'
 
 file '/etc/motd' do
-  content 'This server is AWS'
+  content "This server is AWS
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}
+  "
   action :create
   owner 'root'
   group 'root'
